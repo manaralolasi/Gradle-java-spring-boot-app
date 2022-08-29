16167048e16fea2ad558d166209ccf387da8b07b
manaralolasi <manaralalasi2030@gmail.com>
2022-08-29 09:50:24 +0300

    initial commit

---
+++ Desktop/Gradle-java-spring-boot-app/src/main/java/com/codingdojo/Gradlejavaspringbootapp/GradleJavaSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.Gradlejavaspringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleJavaSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleJavaSpringBootAppApplication.class, args);
+	}
+
+}
---
+++ Desktop/Gradle-java-spring-boot-app/src/test/java/com/codingdojo/Gradlejavaspringbootapp/GradleJavaSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.Gradlejavaspringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleJavaSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
